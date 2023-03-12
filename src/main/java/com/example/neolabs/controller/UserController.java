12459becbd570516a72cf4685fb69686beeb2ebc
Asel Esra Ozyilmaz <esra.ozyilmaz@iaau.edu.kg>
2023-03-13 00:22:19 +0600

    fixed mapping type for exporting files

--- src/main/java/com/example/neolabs/controller/UserController.java
+++ src/main/java/com/example/neolabs/controller/UserController.java
@@ -13,10 +13,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -31,7 +28,7 @@ public class UserController extends BaseController {
     private final CsvExportService csvExportService;
 
 
-    @RequestMapping(path = "/download")
+    @GetMapping(path = "/download")
     public void getAllEmployeesInCsv(HttpServletResponse servletResponse) throws IOException {
         servletResponse.setContentType("text/csv");
         servletResponse.addHeader("Content-Disposition","attachment; filename=\"users.csv\"");
