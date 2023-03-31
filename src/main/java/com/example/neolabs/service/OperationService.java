4c7f2eaade12370f771b61bc0e4cf93017ea5d32
Sakubek <salamatbek10@gmail.com>
2023-03-31 18:03:53 +0600

    added operation recorder for every entity

--- src/main/java/com/example/neolabs/service/OperationService.java
+++ src/main/java/com/example/neolabs/service/OperationService.java
@@ -1,7 +1,7 @@
 package com.example.neolabs.service;
 
 import com.example.neolabs.dto.OperationDto;
-import com.example.neolabs.entity.Application;
+import com.example.neolabs.entity.*;
 import com.example.neolabs.enums.OperationType;
 
 import java.util.List;
@@ -9,6 +9,13 @@ import java.util.List;
 public interface OperationService {
 
     void recordApplicationOperation(Application application, OperationType operationType);
+    void recordStudentOperation(Student student, OperationType operationType);
+    void recordGroupOperation(Group group, OperationType operationType);
+    void recordCourseOperation(Course course, OperationType operationType);
+    void recordUserOperation(User user, OperationType operationType);
+    void recordMentorOperation(Mentor mentor, OperationType operationType);
+    void recordDepartmentOperation(Department department, OperationType operationType);
+    void recordPaymentOperation(Payment payment, OperationType operationType);
 
     List<OperationDto> getAllOperations(boolean includeApplications,
                                         boolean includeStudents,
--- src/main/java/com/example/neolabs/service/impl/OperationServiceImpl.java
+++ src/main/java/com/example/neolabs/service/impl/OperationServiceImpl.java
@@ -1,8 +1,7 @@
 package com.example.neolabs.service.impl;
 
 import com.example.neolabs.dto.OperationDto;
-import com.example.neolabs.entity.Application;
-import com.example.neolabs.entity.User;
+import com.example.neolabs.entity.*;
 import com.example.neolabs.entity.operation.*;
 import com.example.neolabs.enums.EntityEnum;
 import com.example.neolabs.enums.OperationType;
@@ -46,6 +45,83 @@ public class OperationServiceImpl implements OperationService {
                 .description(operationUtil.buildDescription(operationType, EntityEnum.APPLICATION, application.getId()))
                 .build();
         applicationOpRepo.save(operation);
+    }
+
+    @Override
+    public void recordStudentOperation(Student student, OperationType operationType) {
+        StudentOperation operation = StudentOperation.builder()
+                .user(getCurrentUserEntity())
+                .student(student)
+                .operationType(operationType)
+                .description(operationUtil.buildDescription(operationType, EntityEnum.STUDENT, student.getId()))
+                .build();
+        studentOpRepo.save(operation);
+    }
+
+    @Override
+    public void recordGroupOperation(Group group, OperationType operationType) {
+        GroupOperation operation = GroupOperation.builder()
+                .user(getCurrentUserEntity())
+                .group(group)
+                .operationType(operationType)
+                .description(operationUtil.buildDescription(operationType, EntityEnum.GROUP, group.getId()))
+                .build();
+        groupOpRepo.save(operation);
+    }
+
+    @Override
+    public void recordCourseOperation(Course course, OperationType operationType) {
+        CourseOperation operation = CourseOperation.builder()
+                .user(getCurrentUserEntity())
+                .course(course)
+                .operationType(operationType)
+                .description(operationUtil.buildDescription(operationType, EntityEnum.COURSE, course.getId()))
+                .build();
+        courseOpRepo.save(operation);
+    }
+
+    @Override
+    public void recordUserOperation(User user, OperationType operationType) {
+        UserOperation operation = UserOperation.builder()
+                .user(getCurrentUserEntity())
+                .targetUser(user)
+                .operationType(operationType)
+                .description(operationUtil.buildDescription(operationType, EntityEnum.USER, user.getId()))
+                .build();
+        userOpRepo.save(operation);
+    }
+
+    @Override
+    public void recordMentorOperation(Mentor mentor, OperationType operationType) {
+        MentorOperation operation = MentorOperation.builder()
+                .user(getCurrentUserEntity())
+                .mentor(mentor)
+                .operationType(operationType)
+                .description(operationUtil.buildDescription(operationType, EntityEnum.MENTOR, mentor.getId()))
+                .build();
+        mentorOpRepo.save(operation);
+    }
+
+    @Override
+    public void recordDepartmentOperation(Department department, OperationType operationType) {
+        DepartmentOperation operation = DepartmentOperation.builder()
+                .user(getCurrentUserEntity())
+                .department(department)
+                .operationType(operationType)
+                .description(operationUtil.buildDescription(operationType, EntityEnum.DEPARTMENT, department.getId()))
+                .build();
+        departmentOpRepo.save(operation);
+    }
+
+    @Override
+    public void recordPaymentOperation(Payment payment, OperationType operationType) {
+        PaymentOperation operation = PaymentOperation.builder()
+                .user(getCurrentUserEntity())
+                .payment(payment)
+                .operationType(operationType)
+                .description(operationUtil.buildDescription(operationType, EntityEnum.PAYMENT, payment.getId()))
+                .build();
+        paymentOpRepo.save(operation);
     }
 
 
